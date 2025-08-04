0c0188770b696146d4f0104e99c2324ced46a836
alsrbsqkqk <leadbetterpeterseo@gmail.com>
2025-08-04 09:38:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1369,3 +1369,7 @@ def get_last_trade_time():
             return datetime.fromisoformat(f.read().strip())
     except:
         return None
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
