37770bc615c1eaa4c722bae12b28620ab41c3bac
alsrbsqkqk <leadbetterpeterseo@gmail.com>
2026-04-05 19:39:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1640,7 +1640,7 @@ async def webhook(request: Request):
     strategy_thresholds = {
     "Balance breakout": 4.5,
     "BUY_ENTRY_BAR_CLOSE": 1.0,
-    "SELL_ENTRY_BAR_CLOSE": -2.0,
+    "SELL_ENTRY_BAR_CLOSE": -1.5,
     }
 
     alert_data = payload.get("alert_data", {})
